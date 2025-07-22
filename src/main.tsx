31e7cbbdc88d15dc3405d4d8a15f00357b561d96
วัชรพล นุ้ยสิน <watcharapon.n@psu.ac.th>
2025-07-22 13:13:16 +0700

    ..

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import Day2 from "./page/Day2.tsx";
 import Day3 from "./page/Day3.tsx";
 
 createRoot(document.getElementById("root")!).render(
